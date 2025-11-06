baf882465b2c800754c15359382c80f4b53f723e
Pavel Melnychuk <51413815+Lovesomemama@users.noreply.github.com>
2025-11-06 23:26:58 +0100

    Add comments for better code readability
    Included explanatory comments to functions.

--- Voting.sol
+++ Voting.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.8.0;
 
 contract Voting {
     mapping(address => uint) public votes;
+// Add specified amount of votes to the sender's address
     function vote(uint amount) public {
         votes[msg.sender] += amount;
     }
+// Retrieve vote count for a given address
     function getVotes(address addr) public view returns (uint) {
         return votes[addr];
     }
